e8407981ac0bd3f7151eea9837d81bf9a547a4d1
Ivan Nazarov <yotayopta1@gmail.com>
2021-02-06 20:46:08 +0300

    first commit

--- index.js
+++ index.js
@@ -5,22 +5,46 @@ var material = [
     {id:4, price: 750, name: 'Рваный сланец',src: 'img/RvaniSlanec750.jpeg', description: 'В зависимости от назначения, условий строительства и эксплуатации зданий и сооружений подбираются соответствующие строительные материалы, которые обладают определёнными качествами и защитными свойствами от воздействия на них различной внешней среды. Учитывая эти особенности, любой строительный материал должен обладать определёнными строительно-техническими свойствами.'}
 ]
 
-var toHTML= material.map(value=>`<div class='material-container' id='container'>
-                                    <h1>
-                                        ${value.name}
-                                    </h1>
-                                    <img src="${value.src}" alt="">
-                                    <div>
-                                        <button id="button" data-show='${value.name}'>
-                                            Описание
-                                        </button>
-                                        <div class="description" id="${value.name}">
-                                            ${value.description}
-                                            
+var toHTML= material.map(value=>`<div>
+                                    <div class='material-container' id='container'>
+                                        <h1>
+                                            ${value.name}
+                                        </h1>
+                                        <img src="${value.src}" alt="">
+                                        <div>
+
+                                            <button id="button" data-show='${value.name}'>
+                                                Описание
+                                            </button>
+
+                                            <div class="description" id="${value.name}">
+                                                ${value.description}
+                                                
+                                            </div>
+
+                                        </div>
+                                        <div>
+                                            <h3>Цена: ${value.price} руб/м2</h3>
+                                        </div>
+                                        <div>
+                                            <button id='order' data-order="${value.id}">Заказать</button>
                                         </div>
                                     </div>
-                                    <div>
-                                        <h3>Цена: ${value.price} руб/м2</h3>
+                                    <div id='${value.id}' class='modal'>
+                                        <div class='modal__title'>
+                                            Название товара: ${value.name}
+                                        </div>
+                                        <div class='input1'>
+                                            Ваше имя:<input placeholder="Введите ваше имя"></input>
+                                        </div>
+                                        <div>
+                                            Ваш телефон:<input value='+7'></input>
+                                        </div>
+                                        <div>
+                                            <button id='send'>Отправить</button>
+                                            <button id='cancel' data-cancel="${value.id}">Отменить</button>
+                                        </div>
+                                        
                                     </div>
                                 </div>`)
 
@@ -32,18 +56,28 @@ block.insertAdjacentHTML('beforeend', toHTML)
 var btn = document.getElementById('button')
 
 
+
 document.addEventListener('click', event=>{
     event.preventDefault()
-    
     var show = document.getElementById(event.target.dataset.show)
     
-    if(show.style.display==='block'){
-        show.style.display='none    '
-    }else{
-        show.style.display='block'
-    }
-    
+    $(show).slideToggle(500)
+
 })
 
+document.addEventListener('click',(event)=>{
+    var order = document.getElementById(event.target.dataset.order)
+    
+    $(order).slideToggle(500)
+})
+
+
+
+
+
+document.addEventListener('click', (event)=>{
+    var cancel = document.getElementById(event.target.dataset.cancel)
+    $(cancel).slideToggle(500)
+})
 
 
